bb2e00e33707c519353220950c796a5ae5625782
Sunghah <sunghahh@gmail.com>
2018-03-26 20:00:17 +0900

    debug

--- python/postprocess.py
+++ python/postprocess.py
@@ -5,8 +5,6 @@ import numpy as np
 
 data = '/home/sunghah/thesis/extract.csv'
 
-out_df = pd.DataFrame() # output dataframe
-
 # read into dataframe while specifying praat's '--undefined--' as missing value
 df = pd.read_csv(data, dtype={'filename': str, 'left': str, 'phoneme': str,
                               'right': str, 'start': np.float64, 'end': np.float64,
@@ -35,46 +33,63 @@ vowels = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER',
           'EY', 'IH', 'IY', 'OW', 'OY', 'UH', 'UW', 'W', 'Y'] # all vowels
 
 
+preb_df = pd.DataFrame() # output dataframe
+
+# get pre-boundary phones
+for idx in range(len(df)):
+    print('on line #', idx+1, "/", lines)
+    sys.stdout.flush()
+
+    phone = df.loc[idx, 'phoneme']
+    left = df.loc[idx, 'left']
+    depth = 1
+
+    if phone == silence:
+        # shift left up to 'SIL' or 'Start'
+        while (phone == silence) and (left != 'Start') and (idx-depth > -1):
+
+            if left[0:2] in vowels:
+                df.loc[idx-depth, 'context'] = 'Pre'
+                df.loc[idx-depth, 'depth'] = depth
+                df.loc[idx-depth, 'sil_dur'] = df.loc[idx, 'duration']
+                preb_df = preb_df.append(df.iloc[idx-depth])
+
+            phone = df.loc[idx, 'phoneme']
+            left = df.loc[idx, 'left']
+            right = df.loc[idx, 'right']
+            depth += 1
+
+
+preb_df.to_csv('preb.csv', sep=',', encoding='utf-8', index=False)
+del preb_df
+
+
+postb_df = pd.DataFrame() # output dataframe
+
+# get post-boundary phones
 for idx in range(len(df)):
     print('on line #', idx, "/", lines)
     sys.stdout.flush()
 
     phone = df.loc[idx, 'phoneme']
+    right = df.loc[idx, 'right']
+    depth = 1
+
 
     if phone == silence:
+        # shift right up to 'SIL' or 'Start'
+        while (right == silence) and (right != 'End') and (idx+depth < lines):
 
-        left = df.loc[idx, 'left']
-        depth = 1
-
-        while ((left != 'Start') or (left != silence)) and (idx-depth > -1):
-
-            left = df.loc[idx-depth, 'phoneme'] # pre-boundary phone
-
-            # first two symbols, excluding lexical stress markers and B/I/E tags
-            if left[0:2] in vowels:
-                df.loc[idx-depth, 'context'] = 'Pre'
-                df.loc[idx-depth, 'depth'] = depth
-                df.loc[idx-depth, 'sil_dur'] = df.loc[idx, 'duration']
-                out_df = out_df.append(df.iloc[idx-depth])
-
-            depth += 1
-
-
-        right = df.loc[idx, 'right']
-        depth = 1
-
-        while ((right != 'End') and (right != silence)) and (idx+depth < lines):
-
-            right = df.loc[idx+depth, 'phoneme'] # post-boundary phone
-
-            # first two symbols, excluding lexical stress markers and B/I/E tags
             if right[0:2] in vowels:
                 df.loc[idx+depth, 'context'] = 'Post'
                 df.loc[idx+depth, 'depth'] = depth
                 df.loc[idx+depth, 'sil_dur'] = df.loc[idx, 'duration']
-                out_df = out_df.append(df.iloc[idx+depth])
+                postb_df = postb_df.append(df.iloc[idx+depth])
 
+            phone = df.loc[idx, 'phoneme']
+            left = df.loc[idx, 'left']
+            right = df.loc[idx, 'right']
             depth += 1
 
 
-out_df.to_csv('result_proc.csv', sep=',', encoding='utf-8', index=False)
+postb_df.to_csv('postb.csv', sep=',', encoding='utf-8', index=False)
